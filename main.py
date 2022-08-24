2b0d3e5168766e108be1d147f92c6260a878be84
Microgggfdc <93667066+Microgggfdc@users.noreply.github.com>
2022-08-24 12:45:42 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
+import asyncio
+
+import time
+from pywebio import start_server
+from pywebio.input import *
+from pywebio.output import *
+from pywebio.session import defer_call, info as session_info, run_async, run_js
+
+chat_msgs = []
+online_users = set()
+
+MAX_MESSAGES_COUNT = 100
+
 
 async def main():
     global chat_msgs
